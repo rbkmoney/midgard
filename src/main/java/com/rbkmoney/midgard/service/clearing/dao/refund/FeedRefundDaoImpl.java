9bd1297c600a5c1e5e6de8b9287a7fc03ef41997
Pavel Popov <popoffpavel@yandex.ru>
2019-02-08 16:26:41 +0300

    Add current in condition

--- src/main/java/com/rbkmoney/midgard/service/clearing/dao/refund/FeedRefundDaoImpl.java
+++ src/main/java/com/rbkmoney/midgard/service/clearing/dao/refund/FeedRefundDaoImpl.java
@@ -1,8 +1,8 @@
 package com.rbkmoney.midgard.service.clearing.dao.refund;
 
-import com.rbkmoney.midgard.service.clearing.exception.DaoException;
 import com.rbkmoney.midgard.service.clearing.dao.common.AbstractGenericDao;
 import com.rbkmoney.midgard.service.clearing.dao.common.RecordRowMapper;
+import com.rbkmoney.midgard.service.clearing.exception.DaoException;
 import org.jooq.Query;
 import org.jooq.generated.feed.enums.RefundStatus;
 import org.jooq.generated.feed.tables.pojos.Refund;
@@ -10,7 +10,6 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
-
 import java.util.List;
 
 import static org.jooq.generated.feed.Tables.PAYMENT;
@@ -30,11 +29,15 @@ public class FeedRefundDaoImpl extends AbstractGenericDao implements RefundDao {
     public List<Refund> getRefunds(long eventId, List<Integer> providerIds, int poolSize) throws DaoException {
         Query query = getDslContext().select(REFUND.fields())
                 .from(REFUND)
-                .join(PAYMENT).on(REFUND.INVOICE_ID.eq(PAYMENT.INVOICE_ID))
-                .and(REFUND.PAYMENT_ID.eq(PAYMENT.PAYMENT_ID))
-                .and(PAYMENT.ROUTE_PROVIDER_ID.in(providerIds))
-                .where(REFUND.EVENT_ID.greaterThan(eventId))
-                .and(REFUND.STATUS.eq(RefundStatus.succeeded))
+                .join(PAYMENT).on(
+                        REFUND.INVOICE_ID.eq(PAYMENT.INVOICE_ID)
+                                .and(REFUND.PAYMENT_ID.eq(PAYMENT.PAYMENT_ID))
+                                .and(PAYMENT.ROUTE_PROVIDER_ID.in(providerIds))
+                                .and(REFUND.EVENT_ID.greaterThan(eventId))
+                                .and(REFUND.STATUS.eq(RefundStatus.succeeded))
+                                .and(PAYMENT.CURRENT)
+                                .and(REFUND.CURRENT)
+                )
                 .orderBy(REFUND.EVENT_ID).limit(poolSize);
         return fetch(query, refundRowMapper);
     }
