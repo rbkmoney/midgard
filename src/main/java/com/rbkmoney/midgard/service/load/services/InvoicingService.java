298cf3588da23de0ad0fa3efcd01871857edda6f
Baikov Dmitrii <44803026+D-Baykov@users.noreply.github.com>
2019-02-11 13:12:38 +0300

    Fix the launch event number (#22)

--- src/main/java/com/rbkmoney/midgard/service/load/services/InvoicingService.java
+++ src/main/java/com/rbkmoney/midgard/service/load/services/InvoicingService.java
@@ -5,8 +5,8 @@ import com.rbkmoney.damsel.payment_processing.EventPayload;
 import com.rbkmoney.midgard.service.clearing.exception.DaoException;
 import com.rbkmoney.midgard.service.load.dao.invoicing.iface.InvoiceDao;
 import com.rbkmoney.midgard.service.load.pollers.event_sink.invoicing.AbstractInvoicingHandler;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,13 +15,21 @@ import java.util.List;
 import java.util.Optional;
 
 @Slf4j
-@RequiredArgsConstructor
 @Service
 public class InvoicingService implements EventService<Event, EventPayload> {
 
     private final InvoiceDao invoiceDao;
 
     private final List<AbstractInvoicingHandler> invoicingHandlers;
+
+    public InvoicingService(InvoiceDao invoiceDao,
+                            List<AbstractInvoicingHandler> invoicingHandlers) {
+        this.invoiceDao = invoiceDao;
+        this.invoicingHandlers = invoicingHandlers;
+    }
+
+    @Value("${import.init-last-event-id}")
+    private long initLastEventId;
 
     @Override
     @Transactional(propagation = Propagation.REQUIRED)
@@ -38,7 +46,12 @@ public class InvoicingService implements EventService<Event, EventPayload> {
     public Optional<Long> getLastEventId(int div, int mod) throws DaoException {
         Optional<Long> lastEventId = Optional.ofNullable(invoiceDao.getLastEventId(div, mod));
         log.info("Last invoicing eventId={}", lastEventId);
-        return lastEventId;
+        if (lastEventId.isPresent()) {
+            return lastEventId;
+        } else {
+            log.debug("Last invoicing eventId will set to {}", initLastEventId);
+            return Optional.of(initLastEventId);
+        }
     }
 
     @Override
