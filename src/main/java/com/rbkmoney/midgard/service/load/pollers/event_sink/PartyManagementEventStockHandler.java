adba3ff790e98dea470c066fe3efe107fbe0ca4d
Baikov Dmitrii <44803026+D-Baykov@users.noreply.github.com>
2019-03-20 15:37:15 +0300

    Fixing errors when receiving events (#34)

--- src/main/java/com/rbkmoney/midgard/service/load/pollers/event_sink/PartyManagementEventStockHandler.java
+++ src/main/java/com/rbkmoney/midgard/service/load/pollers/event_sink/PartyManagementEventStockHandler.java
@@ -5,7 +5,7 @@ import com.rbkmoney.damsel.payment_processing.Event;
 import com.rbkmoney.damsel.payment_processing.EventPayload;
 import com.rbkmoney.eventstock.client.EventAction;
 import com.rbkmoney.eventstock.client.EventHandler;
-import com.rbkmoney.midgard.service.load.services.EventService;
+import com.rbkmoney.midgard.service.load.services.PartyManagementService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class PartyManagementEventStockHandler implements EventHandler<StockEvent> {
 
-    private final EventService<Event, EventPayload> partyManagementService;
+    private final PartyManagementService partyManagementService;;
 
     @Override
     public EventAction handle(StockEvent stockEvent, String subsKey) {
