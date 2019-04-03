1fcab7d4ad41c47f3b65e5a1b61f56c5daf3e434
Baikov Dmitrii <44803026+D-Baykov@users.noreply.github.com>
2019-04-03 11:34:17 +0300

    Add "adapter failure" events to clearing revision (#43)

--- src/main/java/com/rbkmoney/midgard/service/clearing/services/ClearingRevisionService.java
+++ src/main/java/com/rbkmoney/midgard/service/clearing/services/ClearingRevisionService.java
@@ -6,14 +6,16 @@ import com.rbkmoney.midgard.service.clearing.data.ClearingProcessingEvent;
 import com.rbkmoney.midgard.service.clearing.handlers.Handler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.jooq.generated.midgard.enums.ClearingEventStatus;
 import org.jooq.generated.midgard.tables.pojos.ClearingEventInfo;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.rbkmoney.midgard.service.clearing.utils.ClearingAdaptersUtils.getClearingAdapter;
+import static org.jooq.generated.midgard.enums.ClearingEventStatus.*;
 
 /** Сервис проверки статуса клиринговых событий
  *
@@ -39,26 +41,27 @@ public class ClearingRevisionService implements GenericService {
     public void process() {
         log.info("Clearing revision process get started");
 
-        List<ClearingEventInfo> startedEvents = eventInfoDao.getAllClearingEvents(ClearingEventStatus.STARTED);
-        log.info("Count of started clearing event is {}", startedEvents.size());
-        runRevision(startedEvents, clearingDataTransferHandler);
+        List<ClearingEventInfo> startedEvents = eventInfoDao.getAllClearingEvents(STARTED);
+        // ADAPTER_FAULT - это ошибка при взаимодействии с клиринговым адаптером.
+        List<ClearingEventInfo> adapterFaultEvents = eventInfoDao.getAllClearingEvents(ADAPTER_FAULT).stream()
+                .filter(event -> event.getDate().getDayOfYear() - LocalDateTime.now().getDayOfYear() == 0)
+                .collect(Collectors.toList());
+        startedEvents.addAll(adapterFaultEvents);
+        log.info("Count of started clearing events is {}", startedEvents.size());
+        startedEvents.forEach(event -> clearingRevision(event, clearingDataTransferHandler));
 
-        List<ClearingEventInfo> executeEvents = eventInfoDao.getAllClearingEvents(ClearingEventStatus.EXECUTE);
-        log.info("Count of executed clearing event is {}", executeEvents.size());
-        runRevision(executeEvents, eventStateRevisionHandler);
+        List<ClearingEventInfo> executeEvents = eventInfoDao.getAllClearingEvents(EXECUTE);
+        log.info("Count of executed clearing events is {}", executeEvents.size());
+        executeEvents.forEach(event -> clearingRevision(event, eventStateRevisionHandler));
 
         log.info("Clearing revision is finished");
-
     }
 
-    private void runRevision(List<ClearingEventInfo> events, Handler<ClearingProcessingEvent> handler) {
+    private void clearingRevision(ClearingEventInfo event, Handler<ClearingProcessingEvent> handler) {
         try {
-            for (ClearingEventInfo event : events) {
-                ClearingAdapter clearingAdapter = getClearingAdapter(adapters, event.getProviderId());
-                ClearingProcessingEvent processingEvent =
-                        new ClearingProcessingEvent(clearingAdapter, event.getId());
-                handler.handle(processingEvent);
-            }
+            ClearingAdapter clearingAdapter = getClearingAdapter(adapters, event.getProviderId());
+            ClearingProcessingEvent processingEvent = new ClearingProcessingEvent(clearingAdapter, event.getId());
+            handler.handle(processingEvent);
         } catch (Exception ex) {
             log.error("Error during a revision", ex);
         }
