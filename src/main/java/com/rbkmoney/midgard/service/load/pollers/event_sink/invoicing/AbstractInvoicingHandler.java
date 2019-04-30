39fb4c77dcef733ecd57698c6c061720b4e2a6d6
d.baykov <d.baykov@rbkmoney.com>
2019-04-30 18:14:18 +0300

    Add Kafka

--- src/main/java/com/rbkmoney/midgard/service/load/pollers/event_sink/invoicing/AbstractInvoicingHandler.java
+++ src/main/java/com/rbkmoney/midgard/service/load/pollers/event_sink/invoicing/AbstractInvoicingHandler.java
@@ -1,7 +1,6 @@
 package com.rbkmoney.midgard.service.load.pollers.event_sink.invoicing;
 
 import com.rbkmoney.damsel.payment_processing.InvoiceChange;
-import com.rbkmoney.machinegun.eventsink.MachineEvent;
 import com.rbkmoney.midgard.service.load.model.SimpleEvent;
 import com.rbkmoney.midgard.service.load.pollers.event_sink.Handler;
 
