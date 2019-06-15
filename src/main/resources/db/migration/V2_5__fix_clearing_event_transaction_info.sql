2e3ce87f52b454d8fb8c6c63864ec264a7c4ff30
Baikov Dmitrii <44803026+D-Baykov@users.noreply.github.com>
2019-06-15 11:53:50 +0300

    Fix incorrect getting transactions (#51)

--- src/main/resources/db/migration/V2_5__fix_clearing_event_transaction_info.sql
+++ src/main/resources/db/migration/V2_5__fix_clearing_event_transaction_info.sql
@@ -1,3 +1,2 @@
 ALTER TABLE midgard.clearing_event_transaction_info ADD COLUMN invoice_id CHARACTER VARYING;
 ALTER TABLE midgard.clearing_event_transaction_info ADD COLUMN payment_id CHARACTER VARYING;
-
