966701885ad03337b7da347cdcffc7bf66485899
d.baykov <d.baykov@rbkmoney.com>
2019-07-04 17:25:02 +0300

    Fix transaction migration after adding kafka changes [11]

--- src/main/java/com/rbkmoney/midgard/service/load/pollers/listeners/InvoicingKafkaListener.java
+++ src/main/java/com/rbkmoney/midgard/service/load/pollers/listeners/InvoicingKafkaListener.java
@@ -10,9 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.stereotype.Component;
 
-@Component
 @Slf4j
 @RequiredArgsConstructor
 public class InvoicingKafkaListener {
