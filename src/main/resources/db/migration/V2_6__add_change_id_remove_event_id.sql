5ab71d2d11d35ba853b72f9d740f6ca1a886e726
d.baykov <d.baykov@rbkmoney.com>
2019-07-18 14:09:20 +0300

    Fix transaction migration. Correction migration scripts for feed tables [2]

--- src/main/resources/db/migration/V2_6__add_change_id_remove_event_id.sql
+++ src/main/resources/db/migration/V2_6__add_change_id_remove_event_id.sql
@@ -1,19 +1,23 @@
+DO $$
+BEGIN
 /** Modificate Invoice table */
-IF NOT EXIST (SELECT column_name
-              FROM INFORMATION_SCHEMA.COLUMNS
-              WHERE table_schema = 'feed' AND table_name = 'invoice' AND column_name = 'sequence_id'))
-    ALTER TABLE feed.invoice ALTER COLUMN event_id DROP NOT NULL;
-    ALTER TABLE feed.invoice ADD COLUMN sequence_id BIGINT;
-    ALTER TABLE feed.invoice ADD COLUMN change_id INT;
-    UPDATE feed.invoice SET sequence_id = event_id, change_id = id;
-    ALTER TABLE feed.invoice ADD CONSTRAINT invoice_uniq UNIQUE (invoice_id, sequence_id, change_id);
-ENF IF;
+ IF ((SELECT count(column_name)
+     FROM INFORMATION_SCHEMA.COLUMNS
+     WHERE table_schema = 'feed' AND table_name = 'invoice' AND column_name = 'sequence_id') = 0)
+ THEN
+     ALTER TABLE feed.invoice ALTER COLUMN event_id DROP NOT NULL;
+     ALTER TABLE feed.invoice ADD COLUMN sequence_id BIGINT;
+     ALTER TABLE feed.invoice ADD COLUMN change_id INT;
+     UPDATE feed.invoice SET sequence_id = event_id, change_id = id;
+     ALTER TABLE feed.invoice ADD CONSTRAINT invoice_uniq UNIQUE (invoice_id, sequence_id, change_id);
+ END IF;
 
 
 /** Modificate Payment table */
-IF NOT EXIST (SELECT column_name
-              FROM INFORMATION_SCHEMA.COLUMNS
-              WHERE table_schema = 'feed' AND table_name = 'payment' AND column_name = 'sequence_id'))
+ IF ((SELECT count(column_name)
+     FROM INFORMATION_SCHEMA.COLUMNS
+     WHERE table_schema = 'feed' AND table_name = 'payment' AND column_name = 'sequence_id') = 0)
+THEN
     ALTER TABLE feed.payment ALTER COLUMN event_id DROP NOT NULL;
     ALTER TABLE feed.payment ADD COLUMN sequence_id BIGINT;
     ALTER TABLE feed.payment ADD COLUMN change_id INT;
@@ -22,52 +26,59 @@ IF NOT EXIST (SELECT column_name
 END IF;
 
 
-/** Modificate Refund table */
-IF NOT EXIST (SELECT column_name
-              FROM INFORMATION_SCHEMA.COLUMNS
-              WHERE table_schema = 'feed' AND table_name = 'refund' AND column_name = 'sequence_id'))
-    ALTER TABLE feed.refund ALTER COLUMN event_id DROP NOT NULL;
-    ALTER TABLE feed.refund ADD COLUMN sequence_id BIGINT;
-    ALTER TABLE feed.refund ADD COLUMN change_id INT;
-    UPDATE feed.refund SET sequence_id = event_id, change_id = id;
-    ALTER TABLE feed.refund ADD CONSTRAINT refund_uniq UNIQUE (invoice_id, sequence_id, change_id);
-END IF;
+ /** Modificate Refund table */
+ IF ((SELECT count(column_name)
+      FROM INFORMATION_SCHEMA.COLUMNS
+      WHERE table_schema = 'feed' AND table_name = 'refund' AND column_name = 'sequence_id') = 0)
+ THEN
+     ALTER TABLE feed.refund ALTER COLUMN event_id DROP NOT NULL;
+     ALTER TABLE feed.refund ADD COLUMN sequence_id BIGINT;
+     ALTER TABLE feed.refund ADD COLUMN change_id INT;
+     UPDATE feed.refund SET sequence_id = event_id, change_id = id;
+     ALTER TABLE feed.refund ADD CONSTRAINT refund_uniq UNIQUE (invoice_id, sequence_id, change_id);
+ END IF;
 
 
-/** Modificate adjustment table */
-IF NOT EXIST (SELECT column_name
-              FROM INFORMATION_SCHEMA.COLUMNS
-              WHERE table_schema = 'feed' AND table_name = 'adjustment' AND column_name = 'sequence_id'))
-    ALTER TABLE feed.adjustment ALTER COLUMN event_id DROP NOT NULL;
-    ALTER TABLE feed.adjustment ADD COLUMN sequence_id BIGINT;
-    ALTER TABLE feed.adjustment ADD COLUMN change_id INT;
-    UPDATE feed.adjustment SET sequence_id = event_id, change_id = id;
-    ALTER TABLE feed.adjustment ADD CONSTRAINT adjustment_uniq UNIQUE (invoice_id, sequence_id, change_id);
-END IF;
+ /** Modificate adjustment table */
+ IF ((SELECT count(column_name)
+      FROM INFORMATION_SCHEMA.COLUMNS
+      WHERE table_schema = 'feed' AND table_name = 'adjustment' AND column_name = 'sequence_id') = 0)
+ THEN
+     ALTER TABLE feed.adjustment ALTER COLUMN event_id DROP NOT NULL;
+     ALTER TABLE feed.adjustment ADD COLUMN sequence_id BIGINT;
+     ALTER TABLE feed.adjustment ADD COLUMN change_id INT;
+     UPDATE feed.adjustment SET sequence_id = event_id, change_id = id;
+     ALTER TABLE feed.adjustment ADD CONSTRAINT adjustment_uniq UNIQUE (invoice_id, sequence_id, change_id);
+ END IF;
 
 
-/** Modificate clearing_transaction table */
-IF NOT EXIST (SELECT column_name
-              FROM INFORMATION_SCHEMA.COLUMNS
-              WHERE table_schema = 'midgard' AND table_name = 'clearing_transaction' AND column_name = 'sequence_id'))
-    ALTER TABLE midgard.clearing_transaction DROP CONSTRAINT clearing_transaction_PK;
-    ALTER TABLE midgard.clearing_transaction ALTER COLUMN event_id DROP NOT NULL;
-    ALTER TABLE midgard.clearing_transaction ADD COLUMN sequence_id BIGINT;
-    ALTER TABLE midgard.clearing_transaction ADD COLUMN change_id INT;
-    UPDATE midgard.clearing_transaction SET sequence_id = event_id, change_id = event_id;
-    ALTER TABLE midgard.clearing_transaction ADD CONSTRAINT clearing_transaction_uniq UNIQUE (invoice_id, sequence_id, change_id);
-END IF;
+ /** Modificate clearing_transaction table */
+ IF ((SELECT count(column_name)
+      FROM INFORMATION_SCHEMA.COLUMNS
+      WHERE table_schema = 'midgard' AND table_name = 'clearing_transaction' AND column_name = 'sequence_id') = 0)
+ THEN
+     ALTER TABLE midgard.clearing_transaction DROP CONSTRAINT clearing_transaction_PK;
+     ALTER TABLE midgard.clearing_transaction ALTER COLUMN event_id DROP NOT NULL;
+     ALTER TABLE midgard.clearing_transaction ADD COLUMN sequence_id BIGINT;
+     ALTER TABLE midgard.clearing_transaction ADD COLUMN change_id INT;
+     UPDATE midgard.clearing_transaction SET sequence_id = event_id, change_id = event_id;
+     ALTER TABLE midgard.clearing_transaction ADD CONSTRAINT clearing_transaction_uniq UNIQUE (invoice_id, sequence_id, change_id);
+ END IF;
 
 
-/** Modificate clearing_refund table */
-IF NOT EXIST (SELECT column_name
-              FROM INFORMATION_SCHEMA.COLUMNS
-              WHERE table_schema = 'midgard' AND table_name = 'clearing_refund' AND column_name = 'sequence_id'))
-    ALTER TABLE midgard.clearing_refund DROP CONSTRAINT clearing_refund_pkey;
-    ALTER TABLE midgard.clearing_refund ALTER COLUMN event_id DROP NOT NULL;
-    ALTER TABLE midgard.clearing_refund ADD COLUMN sequence_id BIGINT;
-    ALTER TABLE midgard.clearing_refund ADD COLUMN change_id INT;
-    UPDATE midgard.clearing_refund SET sequence_id = event_id, change_id = event_id;
-    ALTER TABLE midgard.clearing_refund ADD CONSTRAINT clearing_refund_uniq UNIQUE (invoice_id, sequence_id, change_id);
-END IF;
+ /** Modificate clearing_refund table */
+ IF ((SELECT count(column_name)
+      FROM INFORMATION_SCHEMA.COLUMNS
+      WHERE table_schema = 'midgard' AND table_name = 'clearing_refund' AND column_name = 'sequence_id') = 0)
+ THEN
+     ALTER TABLE midgard.clearing_refund DROP CONSTRAINT clearing_refund_pkey;
+     ALTER TABLE midgard.clearing_refund ALTER COLUMN event_id DROP NOT NULL;
+     ALTER TABLE midgard.clearing_refund ADD COLUMN sequence_id BIGINT;
+     ALTER TABLE midgard.clearing_refund ADD COLUMN change_id INT;
+     UPDATE midgard.clearing_refund SET sequence_id = event_id, change_id = event_id;
+     ALTER TABLE midgard.clearing_refund ADD CONSTRAINT clearing_refund_uniq UNIQUE (invoice_id, sequence_id, change_id);
+ END IF;
+
+END; $$
+LANGUAGE plpgsql;
 
