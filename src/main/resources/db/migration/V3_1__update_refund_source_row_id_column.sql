b7ad3096a9f229d1d61e0d7f44d71bf6722c67ba
d.baykov <d.baykov@rbkmoney.com>
2019-07-22 19:52:05 +0300

    Fix master branch after changes

--- src/main/resources/db/migration/V3_1__update_refund_source_row_id_column.sql
+++
@@ -1,7 +0,0 @@
-UPDATE midgard.clearing_refund
-SET source_row_id = refund.id
-FROM feed.refund
-WHERE clearing_refund.invoice_id = refund.invoice_id
-  AND clearing_refund.payment_id = refund.payment_id
-  AND refund.status = 'succeeded'
-  and refund.current;
