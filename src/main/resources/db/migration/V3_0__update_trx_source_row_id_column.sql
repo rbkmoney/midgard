4f6b5b44fb6c8091dcd230bdb5aead2b6301301b
Baikov Dmitrii <44803026+D-Baykov@users.noreply.github.com>
2019-07-22 16:38:31 +0300

    Fix transaction migration. Redecorate files. Add new adjustment status (#56)

--- src/main/resources/db/migration/V3_0__update_trx_source_row_id_column.sql
+++ src/main/resources/db/migration/V3_0__update_trx_source_row_id_column.sql
@@ -5,11 +5,3 @@ WHERE clearing_transaction.invoice_id = payment.invoice_id
   AND clearing_transaction.payment_id = payment.payment_id
   AND payment.status = 'captured'
   and payment.current;
-
-UPDATE midgard.clearing_refund
-SET source_row_id = refund.id
-FROM feed.refund
-WHERE clearing_refund.invoice_id = refund.invoice_id
-  AND clearing_refund.payment_id = refund.payment_id
-  AND refund.status = 'succeeded'
-  and refund.current;
