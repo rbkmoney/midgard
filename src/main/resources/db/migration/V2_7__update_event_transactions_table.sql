4b9707b7705b2421a9f2e2724b5b55f462bf8bf9
Baikov Dmitrii <44803026+D-Baykov@users.noreply.github.com>
2019-07-22 20:16:36 +0300

    Fix master branch after changes (#58)

---
+++ src/main/resources/db/migration/V2_7__update_event_transactions_table.sql
@@ -0,0 +1,17 @@
+UPDATE midgard.clearing_event_transaction_info
+SET invoice_id = payment.invoice_id,
+    payment_id = payment.payment_id
+FROM feed.payment
+WHERE clearing_event_transaction_info.invoice_id is null
+  and clearing_event_transaction_info.transaction_type = 'PAYMENT'
+  and payment.session_payload_transaction_bound_trx_id = clearing_event_transaction_info.transaction_id
+  and payment.current;
+
+UPDATE midgard.clearing_event_transaction_info
+SET invoice_id = refund.invoice_id,
+    payment_id = refund.payment_id
+FROM feed.refund
+WHERE clearing_event_transaction_info.invoice_id is null
+  and clearing_event_transaction_info.transaction_type = 'REFUND'
+  and refund.session_payload_transaction_bound_trx_id = clearing_event_transaction_info.transaction_id
+  and refund.current;
