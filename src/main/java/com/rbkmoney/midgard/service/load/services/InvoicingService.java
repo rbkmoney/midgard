aaabca0fe8f02af129b74381575561cc1b825c85
d.baykov <d.baykov@rbkmoney.com>
2019-11-27 16:00:29 +0300

    BJ-654: remove backOff

--- src/main/java/com/rbkmoney/midgard/service/load/services/InvoicingService.java
+++ src/main/java/com/rbkmoney/midgard/service/load/services/InvoicingService.java
@@ -2,14 +2,12 @@ package com.rbkmoney.midgard.service.load.services;
 
 import com.rbkmoney.damsel.payment_processing.EventPayload;
 import com.rbkmoney.damsel.payment_processing.InvoiceChange;
-import com.rbkmoney.midgard.service.load.model.SimpleEvent;
 import com.rbkmoney.midgard.service.load.handler.invoicing.AbstractInvoicingHandler;
+import com.rbkmoney.midgard.service.load.model.SimpleEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.util.backoff.BackOffExecution;
-import org.springframework.util.backoff.ExponentialBackOff;
 
 import java.util.List;
 
@@ -20,12 +18,8 @@ public class InvoicingService implements EventService<SimpleEvent, EventPayload>
 
     private final List<AbstractInvoicingHandler> invoicingHandlers;
 
-    @Value("${import.init-last-event-id}")
-    private long initLastEventId;
-
-    private final ExponentialBackOff backOff = new ExponentialBackOff();
-
-    private BackOffExecution execution = backOff.start();
+    @Value("${clearing-service.error-timeout}")
+    private long errorTimeout = 1000L;
 
     @Override
     public void handleEvents(SimpleEvent simpleEvent, EventPayload payload) {
@@ -41,25 +35,20 @@ public class InvoicingService implements EventService<SimpleEvent, EventPayload>
                     }
                 }
             }
-            resetBackoff();
         } catch (Throwable e) {
             log.error("Unexpected error while handling event with machineId='{}' and eventId='{}' received from {}",
                     simpleEvent.getSourceId(), simpleEvent.getEventId(), simpleEvent.getEventSourceName(), e);
-            waitBackoff(execution);
+            errorWait(errorTimeout);
             throw new RuntimeException("Unexpected error while handling event with machineId='" +
                     simpleEvent.getSourceId() + "' and eventId='" + simpleEvent.getEventId() + "'");
         }
     }
 
-    private BackOffExecution resetBackoff() {
-        return backOff.start();
-    }
-
-    private void waitBackoff(BackOffExecution execution) {
+    private void errorWait(long waitTime) {
         try {
-            Thread.sleep(execution.nextBackOff());
+            Thread.sleep(waitTime);
         } catch (InterruptedException ex) {
-            log.error("PreloadListenerImpl InterruptedException when wait retry e: ", ex);
+            log.error("InvoicingService InterruptedException when wait retry", ex);
             Thread.currentThread().interrupt();
         }
     }
