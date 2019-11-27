9d2312c15d0fa1060539f4f65458f2a2618c6696
Baikov Dmitrii <44803026+D-Baykov@users.noreply.github.com>
2019-11-27 16:14:07 +0300

    BJ-654: Add error polling timeout (#90)

--- src/main/java/com/rbkmoney/midgard/service/load/services/InvoicingService.java
+++ src/main/java/com/rbkmoney/midgard/service/load/services/InvoicingService.java
@@ -2,8 +2,8 @@ package com.rbkmoney.midgard.service.load.services;
 
 import com.rbkmoney.damsel.payment_processing.EventPayload;
 import com.rbkmoney.damsel.payment_processing.InvoiceChange;
-import com.rbkmoney.midgard.service.load.model.SimpleEvent;
 import com.rbkmoney.midgard.service.load.handler.invoicing.AbstractInvoicingHandler;
+import com.rbkmoney.midgard.service.load.model.SimpleEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,8 +18,8 @@ public class InvoicingService implements EventService<SimpleEvent, EventPayload>
 
     private final List<AbstractInvoicingHandler> invoicingHandlers;
 
-    @Value("${import.init-last-event-id}")
-    private long initLastEventId;
+    @Value("${clearing-service.error-timeout}")
+    private long errorTimeout = 1000L;
 
     @Override
     public void handleEvents(SimpleEvent simpleEvent, EventPayload payload) {
@@ -38,8 +38,18 @@ public class InvoicingService implements EventService<SimpleEvent, EventPayload>
         } catch (Throwable e) {
             log.error("Unexpected error while handling event with machineId='{}' and eventId='{}' received from {}",
                     simpleEvent.getSourceId(), simpleEvent.getEventId(), simpleEvent.getEventSourceName(), e);
+            errorWait(errorTimeout);
             throw new RuntimeException("Unexpected error while handling event with machineId='" +
                     simpleEvent.getSourceId() + "' and eventId='" + simpleEvent.getEventId() + "'");
+        }
+    }
+
+    private void errorWait(long waitTime) {
+        try {
+            Thread.sleep(waitTime);
+        } catch (InterruptedException ex) {
+            log.error("InvoicingService InterruptedException when wait retry", ex);
+            Thread.currentThread().interrupt();
         }
     }
 
