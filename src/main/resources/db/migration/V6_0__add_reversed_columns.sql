eab77cecd29baead8ee27aece312ecdf52e86d2f
d.baykov <d.baykov@rbkmoney.com>
2021-03-16 15:19:17 +0300

    BJ-1037: Redecorate 6.0 migration file

--- src/main/resources/db/migration/V6_0__add_reversed_columns.sql
+++ src/main/resources/db/migration/V6_0__add_reversed_columns.sql
@@ -1,3 +1,3 @@
-ALTER TABLE midgard.clearing_transaction ADD COLUMN is_reversed BOOLEAN DEFAULT false;
+ALTER TABLE midgard.clearing_transaction ADD COLUMN is_reversed BOOLEAN;
 
-ALTER TABLE midgard.clearing_refund ADD COLUMN is_reversed BOOLEAN DEFAULT false;
+ALTER TABLE midgard.clearing_refund ADD COLUMN is_reversed BOOLEAN;
